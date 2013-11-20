b57ffeaf42a0bb06b51c0ce55d11b96a12a1e679
wxianfeng <wang.fl1429@gmail.com>
2013-11-20 23:36:46 +0800

    charge

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -52,35 +52,60 @@ class PagesController < ApplicationController
     render :json => { code: 1 }
   end
 
-  # Parameters: {"fname"=>"", "lname"=>"", "email"=>"", "phone"=>"", "company"=>"", "position"=>"", "website"=>"", "message"=>"", 
-  # "card_name"=>"", "card_number"=>"", "month"=>"", "year"=>"", "cvv"=>"", "address"=>"", "city"=>"", "state"=>"", "zip_code"=>""}
+  # Parameters: {
+  # "fname"=>"", "lname"=>"", "email"=>"", "phone"=>"", "company"=>"", "position"=>"", "website"=>"", "message"=>"", 
+  # "card_name"=>"", "card_number"=>"", "month"=>"", "year"=>"", "cvv"=>"", "address"=>"", "city"=>"", "state"=>"", 
+  # "zip_code"=>""
+  # }
   def do
+    # customer_details = {
+    #   number: '5520000000000000',
+    #   expiry_month: "12", 
+    #   expiry_year: "2014", 
+    #   cvc: "123", 
+    #   name: 'Roland Robot',
+    #   address_line1: '123 fake street', 
+    #   address_city: 'Melbourne', 
+    #   address_postcode: '1234', 
+    #   address_state: 'Vic', 
+    #   address_country: 'Australia'
+    # }
     customer_details = {
-      # number: params[:card_number],
-      number: '5520000000000000',
-      expiry_month: "12", 
-      expiry_year: "2014", 
-      cvc: "123", 
-      name: 'Roland Robot',
-      address_line1: '123 fake street', 
-      address_city: 'Melbourne', 
-      address_postcode: '1234', 
-      address_state: 'Vic', 
+      number: params[:card_number],
+      expiry_month: params[:month], 
+      expiry_year: params[:year], 
+      cvc: params[:cvv], 
+      name: params[:card_name],
+      address_line1: params[:address], 
+      address_city: params[:city], 
+      address_postcode: params[:zip_code], 
+      address_state: params[:state], 
       address_country: 'Australia'
     }
     begin
       customer = Pin::Customer.create('email@example.com', customer_details)
-    rescue Pin::InvalidResource
-      render :json => { code: 0, error: 'card validate error' } and return
+    rescue # Pin::InvalidResource
+      render :json => { code: 0, error: 'card validate error, please check your billing information' } and return
     end
     Rails.logger.debug "debug customer #{customer.inspect}"
     token = customer['token']
+    type = params[:type]
+    fee = if type == "b"
+      99.95
+    elsif type == "c"
+      149.95
+    elsif type == 'd'
+      209.95
+    end
+    if fee.nil?
+      render :json => { code: 0, error: "no fee" } and return
+    end
     charge_details = {
-      email: "email@example.com", 
-      description: "Description", 
-      amount: "100",
+      email: params[:email], 
+      description: params[:message], 
+      amount: fee,
       currency: "AUD", 
-      ip_address: "127.0.0.1", 
+      ip_address: request.ip,
       customer_token: token
     }
     charge = Pin::Charges.create(charge_details)
